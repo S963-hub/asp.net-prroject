3f79e9e89216dad3ee56966554b8029b093ec43f
S963-hub <132394552+S963-hub@users.noreply.github.com>
2024-12-06 18:03:51 +0300

    Add project files.

---
+++ asp.net prroject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp.net_prroject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
